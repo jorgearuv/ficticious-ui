b5de4e3033ac373dc69714359cd040ac96452baf
Jorge Ruvalcaba <jorgeruvalcabav@gmail.com>
2024-07-31 00:47:17 -0600

    chore: removing Button component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,2 @@
-export { Button } from './Button'
-export type { ButtonProps } from './Button'
-
 export { SearchableMenu } from './SearchableMenu'
 export type { SearchableMenuProps } from './SearchableMenu'
