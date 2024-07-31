8f5f16d3da3226e13092fbe23e7d56f42aa014ca
Jorge Ruvalcaba <jorgeruvalcabav@gmail.com>
2024-07-31 12:59:12 -0600

    chore: cleanup files

--- .storybook/preview.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Preview } from '@storybook/react';
-import '../src/index.css';
-declare const preview: Preview;
-export default preview;
-//# sourceMappingURL=preview.d.ts.map
--- .storybook/theme.d.ts
+++
@@ -1,4 +0,0 @@
-import { ThemeVars } from '@storybook/theming';
-declare const _default: ThemeVars;
-export default _default;
-//# sourceMappingURL=theme.d.ts.map
